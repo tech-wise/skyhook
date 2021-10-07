aaa7762188c2e6ece8e5797b15d78798c90a9835
tech-wise <technologywisdom99@gmail.com>
2021-10-07 10:09:47 -0500

    FAQs and Customer Stories

--- wp-content/themes/skyhook/page-customer.php
+++ wp-content/themes/skyhook/page-customer.php
@@ -1,92 +1,177 @@
 <?php get_header(); ?>
 <!---------------------------------------------- solution banner  section-1  start ---------------------------->
 <!-- Learn Banner Start -->
-	<section class="learn-banner">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 page-heading">
-					<h1>Customer Stories</h1>
-				</div>
-			</div>
-		</div>
-	</section>
+<section class="learn-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 page-heading">
+                <h1>Customer Stories</h1>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- Learn Banner End -->
 
 <!--customer listing start-->
 <section class="customer-post">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 left-side-post">
-                <img src="">
+    <div class="container   ">
+        <div class="row ">
+            <div class="col-md-7 left-side-list ">
+                <div class="top-bnr">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Custmor-banner.jpg">
+                </div>
                 <div class="company-logo">
-                    <img src="">
+
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-3.png">
+
                 </div>
-                <div class="fist-pargraf">
-                    <p></p>
+                <div class="fist-pargraf ">
+                    <div class="right-cort">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/right-invert-coma-logo.png">
+
+                    </div>
+                    <div class="compny-paragraph">
+                        <p>ipsum dolor sit amet, consectetur adipiscing elit, sed
+                            tempor incididunt ut labore et dolore magna aliqua. Quis
+                            ipsum suspendisse ultrices gravida. Risus commodo viverra
+                            maecenas accumsan lacus vel facilisis. </p>
+                    </div>
+                    <div class="left-cort">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/invert-coma-logo.png">
+
+                    </div>
                 </div>
-                
+                <div class="pantlies">
+                    <p> --Pantelis Avlonitis ,</p>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/gmt-small-logo.png">
+                </div>
             </div>
-            <div class="col-md-4 right-side-list">
-                <h3>stories</h3>
-                <div class="main-list-box">
-                    <div class="row list-box">
-                        <div class="col-md-4">
-                            <img src="">
+            <div class="col-md-5 right-side-list ">
+                <div class="left-heading">
+                    <h3>Stories</h3>
+                </div>
+                <div class="main-list-box  ">
+                    <div class="row list-box bottom-border ">
+
+                        <div class="col-md-5 first-logo">
+                            <a href="#">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-1.png">
+                            </a>
+                        </div>
+                        <div class="col-md-7  ">
+                            <a href="#">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                                <a href="#">
+                        </div>
+
+                    </div>
+
+                    <div class="row list-box ">
+                        <div class="col-md-5  first-logo ">
+                            <div class="second-logo">
+                                <a href="#">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-2.png">
+                                </a>
+                            </div>
                         </div>
                         <div class="col-md-7">
-                            <p></p>    
+                            <div class="left-para">
+                                <a href="#">
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row  box-color ">
+
+                        <div class="col-md-4 white-logo ">
+                            <a href="#">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/gmt-blue-logo.png">
+                            </a>
+                        </div>
+                        <div class="col-md-7">
+                            <a href="#">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="row list-box bottom-border">
+                        <div class="col-md-5 first-logo">
+                            <a href="#">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-4.png">
+                            </a>
+                        </div>
+                        <div class="col-md-7">
+                            <a href="#">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row list-box bottom-border">
+                        <div class="col-md-5 first-logo">
+                            <div class="fifth-logo">
+                                <a href="#">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-5.png">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <a href="#">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                            </a>
                         </div>
                     </div>
                     <div class="row list-box">
-                        <div class="col-md-4">
-                            <img src="">
+                        <div class="col-md-5 first-logo">
+                            <div class="sixth-logo">
+                                <a href="#">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stories-6.png">
+                                </a>
+                            </div>
                         </div>
                         <div class="col-md-7">
-                            <p></p>    
+                            <div class="left-para">
+                                <a href="#">
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div class="row list-box">
-                        <div class="col-md-4">
-                            <img src="">
-                        </div>
-                        <div class="col-md-7">
-                            <p></p>    
-                        </div>
-                    </div>
-                    <div class="row list-box">
-                        <div class="col-md-4">
-                            <img src="">
-                        </div>
-                        <div class="col-md-7">
-                            <p></p>    
-                        </div>
-                    </div>
-                    <div class="row list-box">
-                        <div class="col-md-4">
-                            <img src="">
-                        </div>
-                        <div class="col-md-7">
-                            <p></p>    
-                        </div>
-                    </div>
-               </div>
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12">
-                <p></p>
-                <div class="row">
+            <div class="col-md-12 fist-pargraf">
+                <div class="mgt-para">
+                    <p>GMT conducts 3D scanning and modeling for component arrangement and piping of competing
+                        systems, pre-selected by the owner.clash detection process is performed for every system in order
+                        to minimize the interference with existing structure and outfitting arrangement.GMT delivers a
+                        detailed feasibility report and after proper system’s selection, an engineering study is conducted
+                        moving forward to completing the installation process.</p>
+                    <p>GMT thoroughly plans the arrangement of SOx scrubber components, in order to be fully customized to the vessel’s exact operational characteristics.</p>
+                </div>
+                <div class="row three-images">
                     <div class="col-md-4">
-                        <img src="">
+
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bulding-image.png">
+
                     </div>
-                    <div class="col-md-4">
-                        <img src="">
+                    <div class="col-md-4 image-spacing">
+
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pipes-image.png">
+
                     </div>
-                    <div class="col-md-4">
-                        <img src="">
+                    <div class="col-md-4 fist-pargraf image-spacing">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/trbine-image.png">
+
                     </div>
                 </div>
-                <p></p>
+                <div class="last-para">
+                    <p>GMT conducts 3D scanning and modeling for component arrangement and piping of competing
+                        systems, pre-selected by the owner.Α clash detection process is performed for every system in
+                        order to minimize the interference with existing structure and outfitting arrangement.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -94,5 +179,6 @@
 <!--customer listing end-->
 
 <!-- solution reports  section-11  end -->
+
 <?php
 get_footer();
--- wp-content/themes/skyhook/page-faqdetail.php
+++ wp-content/themes/skyhook/page-faqdetail.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<!-- Learn Banner Start -->
+<!-- Learn Banner Start
 	<section class="learn-banner">
 		<div class="container">
 			<div class="row">
@@ -11,10 +11,10 @@ get_header();
 				</div>
 			</div>
 		</div>
-	</section>
+	</section>--->
 <!-- Learn Banner End -->
 
-<!--faq list start-->
+<!--faq list start
 <section class="faq-list">
     <div class="container">
         <div class="row">
@@ -27,9 +27,70 @@ get_header();
             </div>
         </div>
     </div>
-</section>
+</section>--->
 <!--faq list end-->
+<!-- Learn Banner Start -->
+	<section class="learn-banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 page-heading">
+					<h1>Questions & Answers</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- Learn Banner End -->
+<!----faq list section--->
+<section class="faq-list mb-5 mt-3">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<h1>Articles Help Center</h1>
+				   <div class="faq-box">				
+					 <h4>i have a part-145 company that i would like to setup with your service?</h4>
+				     <p>Not a problem,Sign up for the 30 days free trail account for the busniesses and if you are                                 satisfied you can explore the subscription plans available.</p>
+				 <div class="divider"> 
+			     </div>
+		     </div>
+		</div>
+			<div class="col-md-4 flex">
+				 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/7ht_24.png" class="img-fluid">
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 mb-5">
+				<div class="list-box">
+				     <h4>Do i need to enter my credit card details to sign up?</h4>
+				      <p>Not a problem,Sign up for the 30 days free trail account for the busniesses and if you are
+					     satisfied you can explore the subscription plans available.</p>
+			    </div>
+			</div>
+		</div>
+	</div>
+	
+      
+</section>
+<!---faq list section end---->
+<!-----get section start---->
+	<section class="get-in-touch pricing">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-md-6 text">
+                <h1>Get in touch with <span>our friendly team.</span></h1>
+                <p>A wealth of styles and elements makes Ollie perfect for building websites for small, medium and large
+                    businesses.</p>
+            </div>
+            <div class="col-xl-5 col-md-6 form skyhook-primary-button">
+                <?php echo do_shortcode('[contact-form-7 id="6" title="Get in Touch"]'); ?>
+            </div>
+        </div>
+    </div>
+</section>
 
+<!-----get section end------>
+<!-----get section end------>
 
 <?php
 get_footer();
